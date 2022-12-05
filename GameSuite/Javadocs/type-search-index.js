b2eaa62f1e7d2f4f4c716e9d71cf11c0ed34b0bc
Iolis1 <99708185+Iolis1@users.noreply.github.com>
2022-12-05 13:04:36 -0500

    Add files via upload

---
+++ GameSuite/Javadocs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"Blackjack"},{"p":"<Unnamed>","l":"Card"},{"p":"<Unnamed>","l":"CurrentFrame"},{"p":"<Unnamed>","l":"Euchre"},{"p":"<Unnamed>","l":"GameSuite"},{"p":"<Unnamed>","l":"JUNIT"},{"p":"<Unnamed>","l":"Player"},{"p":"<Unnamed>","l":"Sudoku"}];updateSearchResults();
